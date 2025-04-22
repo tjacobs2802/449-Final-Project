a141830cdf8fb5e6e40682e9e2b4b09f9f9335ed
Trevor Jacobs <117475995+tjacobs2802@users.noreply.github.com>
2025-04-22 17:49:11 +0000

    added tailwindcss to project

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
