8fa21496855c5dd0d1e7f41a40de914be5ca7d05
Trevor Jacobs <117475995+tjacobs2802@users.noreply.github.com>
2025-04-23 18:40:02 +0000

    added profile pictures and text on user page

--- src/user.jsx
+++ src/user.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import EmptyProfile from './assets/empty_prof.png'
 import { useNavigate } from 'react-router-dom';
 import './user.css'
 import Navbar from './navbar.jsx'
@@ -13,7 +12,14 @@ function User() {
       <Navbar />
 
       <div>
-      This is user page
+      <img src={EmptyProfile} class='h-auto max-w-80 rounded-full' alt="React logo" />
+      <p class='text-xl'>Hello, Trevor!</p>
+      <p class='text-xl'>User Information</p>
+      <p class='text-l'>Height: 6’ 2”</p>
+      <p class='text-l'>Weight: 230 lbs</p>
+      <p class='text-l'>Target Weight: 200lbs</p>
+      <p class='text-l'>AVG Miles Walked/Week: 2.5 mi</p>
+      <p class='text-l'>AVG Calories Consumed/Day: 2500</p>
       <button onClick={() => navigate('/')}>Go to Home Page</button>
       </div>
     </>
