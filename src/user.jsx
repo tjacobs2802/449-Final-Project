172ac5305f25b3d9544124a64716bbddff30aad4
Trevor Jacobs <117475995+tjacobs2802@users.noreply.github.com>
2025-04-24 14:47:09 +0000

    added trophies and got somewhere with data importing on user page

--- src/user.jsx
+++ src/user.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import EmptyProfile from './assets/empty_prof.png'
 import { useNavigate } from 'react-router-dom';
 import './user.css'
 import Navbar from './navbar.jsx'
+import Trophy from './assets/Trophy-Clipart.png'
 import { createClient } from "https://esm.sh/@supabase/supabase-js"
+
+const supabaseUrl = 'https://vhzmoieunypoledibcqa.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoem1vaWV1bnlwb2xlZGliY3FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NjYwMTMsImV4cCI6MjA2MDI0MjAxM30.qLvewkSAwcmg5-7mH10RMz2wGCUlmkz19P00nYjtuzY';
+
 
 
 
 function User() {
   const navigate = useNavigate();
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const { data, error } = await supabase.from('profiles').select('*');
+      if (error) console.error(error);
+      else setProfiles(data);
+    };
+
+    fetchProfiles();
+  }, []);
+
+  const currentUser = profiles[0];
   return (
     <>
       <Navbar />
       <div className='container' class='flex float-left'>
         <div className='profile' class='flex-1'>
           <img src={EmptyProfile} class='h-auto max-w-80 rounded-full' alt="React logo" />
-          <p class='text-4xl'>Hello, Trevor!</p>
-          <p class='text-4xl'>User Information</p>
-          <p class='text-2xl'>Height: 6’ 2”</p>
-          <p class='text-2xl'>Weight: 230 lbs</p>
-          <p class='text-2xl'>Target Weight: 200lbs</p>
-          <p class='text-2xl'>AVG Miles Walked/Week: 2.5 mi</p>
-          <p class='text-2xl'>AVG Calories Consumed/Day: 2500</p>
+          {currentUser ? (
+            <>
+              <p className='text-4xl'>Hello, {currentUser.name}!</p>
+              <p className='text-4xl'>User Information</p>
+              <p className='text-2xl'>Height: {currentUser.height}</p>
+              <p className='text-2xl'>Weight: {currentUser.weight}</p>
+              <p className='text-2xl'>Target Weight: {currentUser.target_weight}</p>
+              <p className='text-2xl'>AVG Miles Walked/Week: {currentUser.average_miles}</p>
+              <p className='text-2xl'>AVG Calories Consumed/Day: {currentUser.average_calories}</p>
+            </>
+          ) : (
+            <p className='text-xl'>Loading user data...</p>
+          )}
           <button onClick={() => navigate('/')}>Go to Home Page</button>
         </div>
         <div className='graph-achievement' class='flex-1, flexbox'>
@@ -31,6 +54,19 @@ function User() {
           </div>
           <div className='achievements'>
             <h2 class='text-3xl'>Achievements</h2>
+            <div class='grid grid-cols-6 gap-4'>
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+              <img src={Trophy} class='h-auto max-w-10' alt="React logo" />
+            </div>
+
           </div>
         </div>
       </div>
