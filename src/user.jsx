810c1d6b7c2dba1cdbc81496242128ed937103b8
Trevor Jacobs <117475995+tjacobs2802@users.noreply.github.com>
2025-04-24 13:01:41 +0000

    made some changes to user page

--- src/user.jsx
+++ src/user.jsx
@@ -4,24 +4,37 @@ import EmptyProfile from './assets/empty_prof.png'
 import { useNavigate } from 'react-router-dom';
 import './user.css'
 import Navbar from './navbar.jsx'
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
+
+
 
 function User() {
   const navigate = useNavigate();
   return (
     <>
       <Navbar />
-
-      <div>
-      <img src={EmptyProfile} class='h-auto max-w-80 rounded-full' alt="React logo" />
-      <p class='text-xl'>Hello, Trevor!</p>
-      <p class='text-xl'>User Information</p>
-      <p class='text-l'>Height: 6’ 2”</p>
-      <p class='text-l'>Weight: 230 lbs</p>
-      <p class='text-l'>Target Weight: 200lbs</p>
-      <p class='text-l'>AVG Miles Walked/Week: 2.5 mi</p>
-      <p class='text-l'>AVG Calories Consumed/Day: 2500</p>
-      <button onClick={() => navigate('/')}>Go to Home Page</button>
+      <div className='container' class='flex float-left'>
+        <div className='profile' class='flex-1'>
+          <img src={EmptyProfile} class='h-auto max-w-80 rounded-full' alt="React logo" />
+          <p class='text-4xl'>Hello, Trevor!</p>
+          <p class='text-4xl'>User Information</p>
+          <p class='text-2xl'>Height: 6’ 2”</p>
+          <p class='text-2xl'>Weight: 230 lbs</p>
+          <p class='text-2xl'>Target Weight: 200lbs</p>
+          <p class='text-2xl'>AVG Miles Walked/Week: 2.5 mi</p>
+          <p class='text-2xl'>AVG Calories Consumed/Day: 2500</p>
+          <button onClick={() => navigate('/')}>Go to Home Page</button>
+        </div>
+        <div className='graph-achievement' class='flex-1, flexbox'>
+          <div className='graph'>
+            <h2 class='text-3xl'>Calorie Intake Graph</h2>
+          </div>
+          <div className='achievements'>
+            <h2 class='text-3xl'>Achievements</h2>
+          </div>
+        </div>
       </div>
+
     </>
   )
 }
