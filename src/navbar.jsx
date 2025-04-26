ddddb2c38d1cb70a3abb9c600101155ff665daeb
Trevor Jacobs <117475995+tjacobs2802@users.noreply.github.com>
2025-04-26 16:37:01 +0000

    increased text size on navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -4,14 +4,14 @@ function Navbar() {
     return (
       <nav className="bg-pine fixed w-full z-20 top-0 start-0">
         <div className="max-w-screen-xl flex flex-wrap justify-between mx-auto p-4">
-        <span className="text-orange self-center text-2x1 dark:text-white" style={{ fontFamily: "'Poetsen One', sans-serif" }}>
-              Health and Wellness Tracker
-        </span>
+          <span className="text-orange self-center text-3xl dark:text-white" style={{ fontFamily: "'Poetsen One', sans-serif" }}>
+            Health and Wellness Tracker
+          </span>
           <div className="flex space-x-4 ml-auto">
-            <a href="/" className = "text-white dark:text-white hover:underline">Home</a>
-            <a href="/user" className = "text-white dark:text-white hover:underline">Profile</a>
-            <a href="/log_food" className = "text-white dark:text-white hover:underline">Food Log</a>
-            <a href="/leaderboard" className = "text-white dark:text-white hover:underline">Leaderboard</a>
+            <a href="/" className = "text-white dark:text-white hover:underline self-center text-2xl">Home</a>
+            <a href="/user" className = "text-white dark:text-white hover:underline self-center text-2xl">Profile</a>
+            <a href="/log_food" className = "text-white dark:text-white hover:underline self-center text-2xl">Food Log</a>
+            <a href="/leaderboard" className = "text-white dark:text-white hover:underline self-center text-2xl">Leaderboard</a>
           </div>
         </div>
       </nav>
